921000ae9407328beb493cc0c5bca15a6b605593
Ahmad 'Izzan kautsar <ahmadizan2003@gmail.com>
2026-06-11 10:13:57 +0700

    Initial commit

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,7 @@
 select
     id as order_id,
     customer as customer_id,
-    ordered_at,
+    cast(ordered_at as timestamp) as ordered_at,
     cast(ordered_at as date) as ordered_date,
     store_id,
     subtotal,
